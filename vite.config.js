7693bca08067cef3caa21ffd08ccbc743e331de9
ZzelakS <adamsskalezz@gmail.com>
2025-05-05 21:25:26 +0100

    optimized fonts

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    target: 'esnext',           // modern output
+    cssCodeSplit: true,         // split CSS for faster load
+    minify: 'esbuild',          // fast and efficient minifier
+    sourcemap: false,           // disable source maps in prod
   },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true,   // only used in dev
   },
 })
